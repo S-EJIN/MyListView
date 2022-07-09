6bc0947f786e221496945f58c1b97166e753ce47
소 이 진 <tlatms801@naver.com>
2022-07-09 11:25:14 +0900

    리스트뷰의 한 칸에 들어갈 데이터 클래스 생성 - 임시적으로 더미데이터 생성

--- app/src/main/java/com/example/mylistview/MainActivity.kt
+++ app/src/main/java/com/example/mylistview/MainActivity.kt
@@ -4,8 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+//    멤버 변수 선언
+    val mStudenList = ArrayList<StudentData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        학생 데이터(더미 데이터 생성)
+        mStudenList.add(StudentData("김민철1", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철2", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철3", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철4", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철5", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철6", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철7", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철8", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철9", 1990,"서울시 서대문구"))
+        mStudenList.add(StudentData("김민철10", 1990,"서울시 서대문구"))
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mylistview/StudentData.kt
@@ -0,0 +1,7 @@
+package com.example.mylistview
+
+class StudentData (val name : String, val birthYear : Int, val address : String) {
+//위에서 사용하는 것은 클래스 멤버변수(생성자)
+//나이계산하는 부분
+
+}
