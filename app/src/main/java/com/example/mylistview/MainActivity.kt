ad89df8e4cb20890fa858ce7a099552f0aa38658
소 이 진 <tlatms801@naver.com>
2022-07-09 13:53:09 +0900

    adapter class 생성

--- app/src/main/java/com/example/mylistview/MainActivity.kt
+++ app/src/main/java/com/example/mylistview/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mylistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mylistview.adapters.StudentListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //    멤버 변수 선언
     val mStudenList = ArrayList<StudentData>()
+
+//    어댑터 클래스 멤버변수화
+    lateinit var mStudentListAdapter: StudentListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +31,23 @@ class MainActivity : AppCompatActivity() {
         mStudenList.add(StudentData("김민철9", 1990,"서울시 서대문구"))
         mStudenList.add(StudentData("김민철10", 1990,"서울시 서대문구"))
 
+//  실제 어댑터와 ArrayList를 연결(어댑터 클래스를 객체화)
+//        context는 화면을 나타내서 그냥 activity를 던져주면 된다(이화면이야!)
+//        여기서, 리스트 화면에, 만든 데이터를 던진다
+        mStudentListAdapter = StudentListAdapter(this, R.layout.student_list_item, mStudenList)
 
+//        액티비티 화면의 listview 태그와 adapter를 연결
+        mainListView.adapter = mStudentListAdapter
 
+//        리스트뷰 한줄에 대한 클릭 이벤트 처리. i는 한칸 포지션에 대한 인스턴스 번호 = position
+        mainListView.setOnItemClickListener { adapterView, view, position, l ->
+            Toast.makeText(this, "${position}번째 아이템입니다", Toast.LENGTH_SHORT).show()
+        }
 
-
+//        리스트뷰 한 줄에 대한 롱크릭 이벤트 처리
+        mainListView.setOnItemLongClickListener { adapterView, view, i, l ->
+            Toast.makeText(this, "${i}번째 아이템 롱클릭 이벤트", Toast.LENGTH_SHORT).show()
+            return@setOnItemLongClickListener(true)
+        }
     }
 }
--- app/src/main/java/com/example/mylistview/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/example/mylistview/adapters/StudentListAdapter.kt
@@ -1,12 +1,39 @@
 package com.example.mylistview.adapters
 //학생의 목록을 보여주는 adapter
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.mylistview.R
 import com.example.mylistview.StudentData
 
 //    상속 : 이미 만들어진 클래스의 개념을 사용
 class StudentListAdapter(val mContext : Context, val resId : Int, val mList : ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList)
 {
+//    한 칸의 xml에 나타날 모양을 커스터마이징
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//    convertView는 listview의 한 칸이라고 보면 된다
+        var tempRow = convertView
+        if(tempRow == null){
+            //val은 변화가 안되는데 tempRow를 바꾸면 에러가 뜨니 tempRow변수는 val이아닌 var을 사용
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+        }
+        val row = tempRow!!
+//        !! 두개 박으면 null값 안들어간다는 뜻
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+
+//      mList[Position] => mList의 position번째 있는 StudentData
+        nameTxt.text = mList[position].name
+        ageTxt.text = mList[position].birthYear.toString()
+        addressTxt.text = mList[position].address
+
+        return row
+    }
 
 }
