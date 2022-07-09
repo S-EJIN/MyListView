715d7a41cdfa548feaaa233390480702ff914cc8
소 이 진 <tlatms801@naver.com>
2022-07-09 11:46:52 +0900

    adapter class 생성

--- app/src/main/java/com/example/mylistview/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/example/mylistview/adapters/StudentListAdapter.kt
@@ -1,5 +1,12 @@
 package com.example.mylistview.adapters
 //학생의 목록을 보여주는 adapter
-class StudentListAdapter {
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.example.mylistview.StudentData
+
+//    상속 : 이미 만들어진 클래스의 개념을 사용
+class StudentListAdapter(val mContext : Context, val resId : Int, val mList : ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext, resId, mList)
+{
 
 }
